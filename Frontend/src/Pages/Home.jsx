6cb99f0494d9f1517c8c7d55bfba3c766751ec4f
Shrey-9659 <99120228+Shrey-9659@users.noreply.github.com>
2025-01-08 18:40:30 +0530

    Update Home.jsx

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
 const Home = () => {
+  useEffect(() => {
+    fetch("http://localhost:PORT/api/users")
+    .then(rawData => rawData.json())
+    .then(res => console.log(res));
+  }, [])
   return (
     <div>
         <h2>Click on any one of these links to be redirected there</h2>
